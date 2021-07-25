af4975d7572dc630dc6d5ee9d83d28760957ab33
Andrei Vovchok <andreivovchok@bk.ru>
2021-07-25 16:27:30 +0300

    Добавил решение дополнительного задания

---
+++ MyFirstSolution/MyThreeConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyThreeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = 5;
+            Console.WriteLine(test.ToString());
+            //Можно, если использовать метод ToString. Этот метод возвращает string, который является ссылочным типом.
+            //Тогда упаковкане будет происходить.
+        }
+    }
+}
