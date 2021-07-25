951a8ff19f29b2e0ebcd0d056ae4acc12fd8949c
Andrei Vovchok <andreivovchok@bk.ru>
2021-07-25 14:19:35 +0300

    Добавил решение первого и второго задания

---
+++ MyFirstSolution/MyFirstConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyFirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int variable = 10;
+            Console.WriteLine(variable);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyFirstSolution/MySecondConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MySecondConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.141592653;
+            decimal e = 1.414213562373095048m;
+            Console.WriteLine(pi);
+            Console.WriteLine(e);
+        }
+    }
+}
